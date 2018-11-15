a6d5f7125b66358801c17b88f0348c6b2e645220
Daniel <nieltansah@gmail.com>
2018-11-15 22:39:27 +0700

    mlp: 1st try of final MLP class

--- ml_course/mlp.py
+++ ml_course/mlp.py
@@ -14,10 +14,11 @@ def sigmoid_grad(y):
 
 
 class MLP(BaseEstimator, ClassifierMixin):
-    def __init__(self, hidden_layer_sizes=(100, ), momentum=0.0001, learning_rate=0.25):
+    def __init__(self, hidden_layer_sizes=(100, ), momentum=0.0001, learning_rate=0.25, max_iteration=10000):
         self.hidden_layer_sizes = hidden_layer_sizes
         self.momentum = momentum
         self.learning_rate = learning_rate
+        self.max_iteration = max_iteration
 
     # Utilities
 
@@ -50,17 +51,20 @@ class MLP(BaseEstimator, ClassifierMixin):
 
     def _feed_forward(self, X):
         data = X
+        buf_a = self.weight_current_i_
 
         for i in range(len(self.y_)):
             sigma_values = np.sum(
-                data * self.weight_[i], axis=1) + self.weight_bias_[i]
+                data * self.weight_[buf_a][i], axis=1) + self.weight_bias_[buf_a][i]
 
             data = self.y_[i]
             expit(sigma_values, out=data)
 
     def _local_gradient(self, X, y):
+        buf_a = self.weight_current_i_
+
         # Output unit.
-        weights = self.weight_[self.i_output_]
+        weights = self.weight_[buf_a][self.i_output_]
         err_terms = self.err_term_[self.i_output_]
 
         out_y = self.y_[self.i_output_][0]
@@ -73,36 +77,70 @@ class MLP(BaseEstimator, ClassifierMixin):
                 np.expand_dims(err_terms, axis=1) * weights, axis=0)
 
             err_terms = self.err_term_[i]
-            weights = self.weight_[i]
+            weights = self.weight_[buf_a][i]
 
             np.multiply(d_y_arr, sig_arr, out=err_terms)
 
+    def _update_weights(self, X):
+        data = X
+
+        # Swap buffer index
+        buf_a = self.weight_current_i_
+        buf_b = (buf_a + 1) % 2
+
+        self.weight_current_i_ = buf_b
+
+        for i in range(len(self.y_)):
+            # Data.
+            momentum = self.weight_[buf_b][i] * self.momentum
+            learning = self.learning_rate * np.expand_dims(self.err_term_[i], axis=1) * np.expand_dims(data, axis=0)
+
+            weight_ref = self.weight_[buf_a][i]
+            weight_ref += momentum + learning
+
+            # Bias.
+            momentum = self.weight_bias_[buf_b][i] * self.momentum
+            learning = self.learning_rate * self.err_term_[i]
+
+            weight_ref = self.weight_bias_[buf_a][i]
+            weight_ref += momentum + learning
+
+            data = self.y_[i]
+
     def fit(self, X, y):
-        """Fit the model to data matrix X and target(s) y.
-
-        Parameters
-        ----------
-        X : array-like or sparse matrix, shape (n_samples, n_features)
-            The input data.
-
-        y : array-like, shape (n_samples,) or (n_samples, n_outputs)
-            The target values (class labels in classification, real numbers in
-            regression).
-
-        Returns
-        -------
-        self : returns a trained MLP model.
-        """
-
         self.input_size_ = X.shape[1]
 
         dimens = tuple(dim for dim in self._gen_store_dimensions())
         dimens_n_only = tuple((dim[0], ) for dim in dimens)
 
-        self.weight_ = self._gen_store(dimens)
-        self.weight_bias_ = self._gen_store(dimens_n_only)
+        self.weight_current_i_ = 0
+
+        self.weight_ = (self._gen_store(dimens), self._gen_store(dimens),)
+        self.weight_bias_ = (
+            self._gen_store(dimens_n_only), self._gen_store(dimens_n_only))
+
         self.y_ = self._gen_store(dimens_n_only)
         self.err_ = self._gen_store(dimens_n_only)
         self.err_term_ = self._gen_store(dimens_n_only)
 
+        for _ in range(self.max_iteration):
+            err = 0
+            for i in range(X.shape[0]):
+                self._feed_forward(X[i])
+                self._local_gradient(X[i], y[i])
+                self._update_weights(X[i])
+
+                err += (y[i] - self.y_[self.i_output_][0]) ** 2
+
+            self.err_value_ = err * 0.5
+
         return self
+
+    def predict(self, X, y=None):
+        result = np.empty(X.shape[0])
+
+        for i in range(X.shape[0]):
+            self._feed_forward(X[i])
+            result[i] = self.y_[self.i_output_][0]
+
+        return result
